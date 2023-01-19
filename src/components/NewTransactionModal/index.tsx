163541b80af11be7b20d3f0f642efd277aeafd44
Bruna Flor <brn.flor@gmail.com>
2023-01-19 19:41:35 +0000

    modal styled

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,22 +1,24 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Overlay, Content } from "./styles";
+import { X } from "phosphor-react";
+import { Overlay, Content, CloseButton } from "./styles";
 
 export const NewTransactionModal = () => {
   return (
     <Dialog.Portal>
-      <Overlay>
-        <Content>
-          <Dialog.Title>Nova Transação</Dialog.Title>
-          <form action="">
-            <input type="text" placeholder="Descrição" required />
-            <input type="number" placeholder="Preço" required />
-            <input type="text" placeholder="Categoria" required />
+      <Overlay />
+      <Content>
+        <Dialog.Title>Nova Transação</Dialog.Title>
+        <form action="">
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
 
-            <button type="submit">Cadastrar</button>
-          </form>
-          <Dialog.Close />
-        </Content>
-      </Overlay>
+          <button type="submit">Cadastrar</button>
+        </form>
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+      </Content>
     </Dialog.Portal>
   );
 };
