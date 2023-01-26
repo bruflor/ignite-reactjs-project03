2642b3de50062667fc7fd211e83ebba88a10cf48
Bruna Flor <brn.flor@gmail.com>
2023-01-26 20:30:37 +0000

    finishing fopr today

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,6 +3,7 @@ import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formater";
 import {
   PriceHighLight,
   TransactionsContainer,
@@ -35,11 +36,12 @@ export const Transactions = () => {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighLight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === "outcome" && "- "}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighLight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(transaction.createdAt)}</td>
                 </tr>
               );
             })}
